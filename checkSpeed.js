bf5fd1e3ff13099e804fa612adcc2e2013caa92f
kkatalz <zlata.kiev2004@gmail.com>
2025-05-09 16:44:17 +0300

    Implement check speed function.

---
+++ checkSpeed.js
@@ -0,0 +1,17 @@
+// Check limit game.
+// Conditions: Speed limit is 70. For each 5 extra kn we get 1 point. When having 1 points, lisense is suspended.
+// 70, 72  -> OK
+// 75 -> Points : 1; 80 - > Points : 2; 130 ->  Points : 12 and License is suspended.
+
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerHour = 5;
+  if (typeof speed != "number") return NaN;
+  if (speed < speedLimit + kmPerHour) return "Ok";
+
+  const points = Math.floor((speed - speedLimit) / kmPerHour);
+  if (points >= 12) console.log("License is suspended");
+  return "Points: ", points;
+}
+
+console.log(checkSpeed(75));
