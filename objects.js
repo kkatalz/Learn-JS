98c830c9163e242562b7e4a01a634b116a0c019b
kkatalz <zlata.kiev2004@gmail.com>
2025-05-14 19:24:16 +0300

    Implement functions to practice creating objects.

--- objects.js
+++ objects.js
@@ -10,8 +10,6 @@ function showAddress(addressList) {
   }
 }
 
-// showAddress(address);
-
 //Factory function
 function adrressFactory(street, city, zipCode) {
   return {
@@ -22,7 +20,6 @@ function adrressFactory(street, city, zipCode) {
 }
 
 let addressFactory = adrressFactory("Saint Laures, 12", "Kyiv", 1233);
-// console.log(addressFactory);
 
 // Constructor function
 function AddressConstructor(street, city, zipCode) {
@@ -48,3 +45,40 @@ function areEqual(address1, address2) {
 function areSame(address1, address2) {
   return address1 === address2;
 }
+
+// Blog Post Object
+// let blogPost = {
+//   title: "Harry Potter",
+//   body: "tatatatta",
+//   author: "tatata",
+//   views: 2009,
+//   comments: [
+//     { author: "Maclaire Mc.Coffee", body: "Tatatatta" },
+//     { author: "Maclaire Mc.Coffee", body: "Tatatatta" },
+//   ],
+//   isLive: true,
+// };
+// console.log(blogPost);
+
+// Constructor function for Blog Post Object
+
+function BlogConstructor(title, body, author) {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.views = 0;
+  this.comments = [];
+  this.isLive = false;
+}
+
+let blogPost = new BlogConstructor("McMurf", "nanna", "TAtta");
+console.log(blogPost);
+
+// Price Range Object
+let priceRanges = [
+  { label: "$", tooltip: "Inexpensive", minPrice: 0, maxPrice: 10 },
+  { label: "$$", tooltip: "Moderate", minPrice: 11, maxPrice: 20 },
+  { label: "$$$", tooltip: "Expensive", minPrice: 21, maxPrice: 50 },
+];
+
+let restaurants = [{ averagePerPerson: 5 }];
