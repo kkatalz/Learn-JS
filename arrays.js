655a60441d88e3eca10660aad6e2330f296b804c
kkatalz <zlata.kiev2004@gmail.com>
2025-05-16 14:28:17 +0300

    Implement function in two ways to get max number.

--- arrays.js
+++ arrays.js
@@ -80,9 +80,32 @@ function countOccurences(array, number) {
   // 1st way
   //   for (let num of array) if (num === number) occurences++;
   //   return occurences;
+
   // 2nd way
   return array.reduce((accumulator, currentValue) => {
     occurences = currentValue === number ? 1 : 0;
     return accumulator + occurences;
   }, 0);
+}
+
+// Task 6. Get Max
+const numberMaxArray = [-1, -12, -10];
+console.log("\nTask 6. Get Max\nGiven array: ", numberMaxArray);
+
+const max = getMax(numberMaxArray);
+console.log(max);
+
+function getMax(array) {
+  // 1st way
+  if (array.length === 0) return undefined;
+  //   let max = array[0];
+  //   for (let num of array) {
+  //     if (num > max) max = num;
+  //   }
+  //   return max;
+
+  // 2nd way using Reduce method
+  return array.reduce((accumulator, current) =>
+    current > accumulator ? current : accumulator
+  );
 }
