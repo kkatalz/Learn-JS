c8670743b644325016e2daacff680c6d6ff19fba
kkatalz <zlata.kiev2004@gmail.com>
2025-05-16 13:55:07 +0300

    Implement function in two ways to count occurences of a given element in an array.

--- arrays.js
+++ arrays.js
@@ -64,3 +64,25 @@ function move(array, index, offset) {
   outputAfterMove.splice(position, 0, element);
   return outputAfterMove;
 }
+
+// Task 5. Count occurences
+const numbersOccurencesArray = [1, 2, 3, 4, 4, 4, 4];
+console.log(
+  "\nTask 5. Count occurences of number\nGiven array: ",
+  numbersOccurencesArray
+);
+
+const count = countOccurences(numbersOccurencesArray, -11);
+console.log(count);
+
+function countOccurences(array, number) {
+  let occurences = 0;
+  // 1st way
+  //   for (let num of array) if (num === number) occurences++;
+  //   return occurences;
+  // 2nd way
+  return array.reduce((accumulator, currentValue) => {
+    occurences = currentValue === number ? 1 : 0;
+    return accumulator + occurences;
+  }, 0);
+}
