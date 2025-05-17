5396e7904b28d7c95bf8898f85065e4a4813c248
kkatalz <zlata.kiev2004@gmail.com>
2025-05-17 12:58:32 +0300

    Implement circle object with getter for area.

--- functions.js
+++ functions.js
@@ -7,5 +7,19 @@ function sum(...numbers) {
   return numbers.reduce((sum, currentNUmber) => sum + currentNUmber);
 }
 
-console.log(sum(1, 2, 3, 4, 5));
-console.log(sum([1, 2, 3, 4, 5]));
+// console.log(sum(1, 2, 3, 4, 5));
+// console.log(sum([1, 2, 3, 4, 5]));
+
+// Task 2. Area of Circle
+const circle = {
+  center: "O",
+  radius: 2,
+  get area() {
+    return Math.PI * this.radius * this.radius;
+  },
+};
+
+// getter
+console.log(circle.area);
+console.log(circle.radius);
+console.log(circle);
