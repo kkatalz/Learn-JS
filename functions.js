0a6e31b1af555d877062d8c6c699c874150bc466
kkatalz <zlata.kiev2004@gmail.com>
2025-05-17 13:07:12 +0300

    Implement error handling.

--- functions.js
+++ functions.js
@@ -20,6 +20,25 @@ const circle = {
 };
 
 // getter
-console.log(circle.area);
-console.log(circle.radius);
-console.log(circle);
+// console.log(circle.area);
+// console.log(circle.radius);
+// console.log(circle);
+
+// Task 3
+try {
+  const numbers = [1, 2, 3, 4, 4, 4, 4];
+  const count = countOccurences("aaa", 4);
+  console.log(count);
+} catch (e) {
+  console.log(e.message);
+}
+
+function countOccurences(array, number) {
+  if (!Array.isArray(array))
+    throw new Error("Your list of numbers is not an array");
+  let occurences = 0;
+  return array.reduce((accumulator, currentValue) => {
+    occurences = currentValue === number ? 1 : 0;
+    return accumulator + occurences;
+  }, 0);
+}
