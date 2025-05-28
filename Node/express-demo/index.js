eeca15451cc2a1f68b248cc051ebf0e377b298f9
kkatalz <zlata.kiev2004@gmail.com>
2025-05-28 14:06:26 +0300

    Implement DELETE method.

--- Node/express-demo/index.js
+++ Node/express-demo/index.js
@@ -59,6 +59,18 @@ app.put("/api/courses/:id", (req, res) => {
   res.send(course);
 });
 
+// DELETE
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 
