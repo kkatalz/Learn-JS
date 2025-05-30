254d3c38b8e2a182063546436bf3fb5707f1c0c5
kkatalz <zlata.kiev2004@gmail.com>
2025-05-30 17:10:04 +0300

    Solve "callback hell" problem by named functions.

--- async/callback.js
+++ async/callback.js
@@ -1,25 +1,36 @@
-// CAllBACK
-
 console.log("Before");
-getUser(1, (user) => {
-  console.log("User", user);
-
-  getRepositories(user.gitHubUserName, (repos) => {
-    console.log("Repos: ", repos);
-  });
-});
+getUser(1, handleUser);
 console.log("After");
 
-function getUser(id, callbback) {
+function handleUser(user) {
+  getRepositories(user.gitHubUserName, handleRepositories);
+}
+
+function handleRepositories(repos) {
+  getCommits(repos, displayCommits);
+}
+
+function displayCommits(commits) {
+  console.log(commits);
+}
+
+function getUser(id, callback) {
   setTimeout(() => {
     console.log("DB is loading");
-    callbback({ id: id, gitHubUserName: "kkatalz" });
+    callback({ id: id, gitHubUserName: "kkatalz" });
   }, 2000);
 }
 
-function getRepositories(username, callbback) {
+function getRepositories(username, callback) {
   setTimeout(() => {
     console.log("Repositories are being searched..");
-    callbback({ username: username, repos: ["repo1", "repo2", "repo3"] });
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
+
+function getCommits(repos, callback) {
+  setTimeout(() => {
+    console.log("Commits are being fetched..");
+    callback(["commit1", "commit2"]);
   }, 2000);
 }
