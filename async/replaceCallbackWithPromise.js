3351e091060a9bddf15a1f463f3fa24a7fe47d7f
kkatalz <zlata.kiev2004@gmail.com>
2025-05-31 11:16:40 +0300

    Add a draft for async operations using callbacks.

---
+++ async/replaceCallbackWithPromise.js
@@ -0,0 +1,23 @@
+console.log("Before");
+getUser(1, (user) => {
+  getRepositories(user.gitHubUserName, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    });
+  });
+});
+console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("DB is loading");
+    callback({ id: id, gitHubUserName: "kkatalz" });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("Repositories are being searched..");
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
