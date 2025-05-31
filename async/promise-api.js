67312d23217d8852f6722f539b4d149491d21690
kkatalz <zlata.kiev2004@gmail.com>
2025-05-31 11:47:11 +0300

    Implement examplary task for running promises in parallel.

--- async/promise-api.js
+++ async/promise-api.js
@@ -1,7 +1,27 @@
-// Simulate Promise results (e.g., useful for testing)
+// --- Simulate Promise results (e.g., useful for testing) ---
 
 const p = Promise.resolve({ id: 1 });
 p.then((result) => console.log(result));
 
 const pe = Promise.reject(new Error("reason for rejection..."));
 pe.catch((error) => console.log(error));
+
+// --- Running promises in parallel ---
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async 1");
+    resolve(1);
+    // reject(new Error("smth failed"));
+  }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async 2");
+    resolve(2);
+  }, 2000);
+});
+
+Promise.all([p1, p2])
+  .then((result) => console.log(result))
+  .catch((error) => console.log("Error", error.message));
