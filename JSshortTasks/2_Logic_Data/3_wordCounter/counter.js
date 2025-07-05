f45fd0250fbcd06788ec8c5e54cd65e3f845cd72
kkatalz <zlata.kiev2004@gmail.com>
2025-07-05 19:28:33 +0300

    Improve counter.

--- JSshortTasks/2_Logic_Data/3_wordCounter/counter.js
+++ JSshortTasks/2_Logic_Data/3_wordCounter/counter.js
@@ -6,8 +6,9 @@ let fileText = "";
 try {
   fileText = readFileSync(fileToRead, "utf8");
 
-  const words = fileText.split(/\s+/).filter(Boolean).length;
-  const lettersOnly = fileText.replace(/[^a-zA-Z]/g, "").length;
+  const cleanText = fileText.replace(/[.,!?]/g, "");
+  const words = cleanText.split(/\s+/).filter(Boolean).length;
+  const lettersOnly = cleanText.replace(/\s/g, "").length;
 
   console.log(`File: ${fileToRead}`);
   console.log(`Words: ${words}`);
