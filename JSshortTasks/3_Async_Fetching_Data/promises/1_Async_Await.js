988e42ecf8aeb3bbb5098d46b4ba37cc63177082
kkatalz <zlata.kiev2004@gmail.com>
2025-07-07 15:29:15 +0300

    Fix typo (missed return).

--- JSshortTasks/3_Async_Fetching_Data/promises/1_Async_Await.js
+++ JSshortTasks/3_Async_Fetching_Data/promises/1_Async_Await.js
@@ -1,16 +1,20 @@
 function wait(ms) {
   const random = Math.random();
-  if (random < 0.3) Promise.reject("Oops! Random simulated failure");
   console.log(random);
+
+  if (random < 0.3) return Promise.reject("Oops! Random simulated failure");
 
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
   console.log("Waiting for result ...");
-  await wait(3000)
-    .then(() => console.log("3 seonds passed"))
-    .catch(() => console.log("Something bad happened!"));
+  try {
+    await wait(3000);
+    console.log("3 seonds passed");
+  } catch (e) {
+    console.log("Error", e);
+  }
 
   console.log("Done!");
 }
