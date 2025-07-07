6fb012903e431f7ffc22a8b6884baf236296a048
kkatalz <zlata.kiev2004@gmail.com>
2025-07-07 12:28:01 +0300

    Delay a promise with then,catch.

--- JSshortTasks/3_Async_Fetching_Data/promises/1_Async_Await.js
+++ JSshortTasks/3_Async_Fetching_Data/promises/1_Async_Await.js
@@ -1,10 +1,17 @@
 function wait(ms) {
+  const random = Math.random();
+  if (random < 0.3) Promise.reject("Oops! Random simulated failure");
+  console.log(random);
+
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
   console.log("Waiting for result ...");
-  await wait(3000);
+  await wait(3000)
+    .then(() => console.log("3 seonds passed"))
+    .catch(() => console.log("Something bad happened!"));
+
   console.log("Done!");
 }
 
